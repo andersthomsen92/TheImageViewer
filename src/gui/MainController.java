502b56f438f3d7e49b187d679f3c49df5e2a4fad
andersthomsen92 <doomknight92@gmail.com>
2024-03-14 22:21:19 +0100

    Some bluring, some triangles. a little color

--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -1,8 +1,27 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class MainController {
 
-    public void initialize(){
+    @FXML
+    private ImageView imgBackground;
+    @FXML
+    private ImageView imgMainPicture;
 
+
+    public void initialize() {
+
+        BoxBlur blur = new BoxBlur(10,10,20); // Parameters: radius, iterations
+        imgBackground.setEffect(blur);
+        imgBackground.setOpacity(0.4);
     }
+
 }
+
